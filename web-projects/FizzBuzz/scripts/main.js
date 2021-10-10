2b94f8fbde0b846a65031c717453636ad01d1ca8
kyyeo <yeokaiyu@gmail.com>
2021-10-10 23:44:07 +0800

    completed fizzbuzz

--- web-projects/FizzBuzz/scripts/main.js
+++ web-projects/FizzBuzz/scripts/main.js
@@ -1,9 +1,9 @@
+// reference: https://www.theodinproject.com/paths/foundations/courses/foundations/lessons/problem-solving
 
-
+// call this function onload on <body>
 function fizzbuzz() {
     let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
-    let para = document.querySelector("div");
-    let phrase1 = 'FizzBUzz';
+    let phrase1 = 'FizzBuzz';
     let phrase2 = 'Buzz';
     let phrase3 = "Fizz";
 
@@ -11,23 +11,25 @@ function fizzbuzz() {
     for (let i = 1; i <= answer; i++) {
         if(i%3 === 0 && i%5 ===0) {
             console.log(phrase1);
-            let para = document.createElement("p");
-            let text = document.createTextNode(phrase1);
-            para.appendChild(text);
-            continue;
+            createPara(phrase1);
         } else if(i%5 === 0) {
             console.log(phrase2);
-            let text = document.createTextNode(phrase2);
-            para.appendChild(text);
-            continue;
+            createPara(phrase2);
         } else if(i%3 === 0 ) {
             console.log(phrase3);
-            let text = document.createTextNode(phrase3);
-            para.appendChild(text);
-            continue;
+            createPara(phrase3);
         } else {
             console.log(i);
+            createPara(i);
         }
     }
 }
 
+// create a function for repetitive task
+function createPara(phrase) {
+    // reference: https://www.w3schools.com/jsref/met_node_appendchild.asp
+    let para = document.createElement("P");
+    let text = document.createTextNode(phrase);
+    para.appendChild(text);
+    document.querySelector("div").appendChild(para);    
+}
